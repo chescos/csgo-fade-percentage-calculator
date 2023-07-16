5051362a26a4fcea9a02b2e3471240971988b6ff
chescos <chescos@users.noreply.github.com>
2023-07-16 12:32:53 +0200

    revert changes

--- generate.ts
+++ generate.ts
@@ -1,8 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import {
-  FadeCalculator, AmberFadeCalculator, AcidFadeCalculator,
-} from './src';
+import { FadeCalculator, AmberFadeCalculator, AcidFadeCalculator } from './src';
 
 const config = [
   {
@@ -16,7 +14,7 @@ const config = [
   {
     percentages: AcidFadeCalculator.getAllFadePercentages(),
     type: 'acid-fade',
-  }
+  },
 ];
 
 config.forEach((entry) => {
